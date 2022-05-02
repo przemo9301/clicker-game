07a7a538d1a21afc85fabeff3f481096606a406a
Przmysław Łukowiak <przemeklukowiak@gmail.com>
2022-05-02 17:47:22 +0200

    upgrade 2.05.2022

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
-const btn =document.querySelector('button');
+const btnClicker =document.querySelector('.clicker');
 const timer=document.querySelector('.timer')
 const clicker=document.querySelector('.showClicker');
-const btnSub=document.querySelector('.inputButton')
-const inputWindow=document.querySelector('.inputWindow')
+const btnSub=document.querySelector('.inputButton');
+const inputWindow=document.querySelector('.inputWindow');
+const inputName=document.querySelector('.inputName')
+const inputSurname=document.querySelector('.inputSurname')
 
-const newUser= new UserData()
+const newUser= new UserList()
 
 // stałe
 let totalClickValue=0;
@@ -15,19 +17,26 @@ const timeToChoose=[];
 
 
 //przyciski
-btn.addEventListener('click',()=>{
+btnClicker.addEventListener('click',()=>{
     totalClickValue++;
     console.log(totalClickValue);
     if(totalClickValue===1){
         finishTime()
         timeToFinish()
-        newUser.addUserToList()
     }
     
 })
 btnSub.addEventListener('click',(e)=>{
-    e.preventDefault()
-    inputValue=inputWindow.value
+    e.preventDefault();
+    inputValue=inputWindow.value;
+    const playerName =inputName.value;
+    const playerSurname =inputSurname.value;
+    const newPlayer= new User(playerName,playerSurname);
+    newUser.add(newPlayer);
+   
+    
+    
+    
     console.log('dziła', inputValue );
     
 })
--- src/user.js
+++ src/user.js
@@ -2,7 +2,11 @@ class UserList {
     constructor(){
        this.userList=[]
     }
-  
+    add (userId){
+        this.userList.push(userId);
+        console.log(this.userList);
+        
+    }
 } 
 class User {
     constructor(name,surname){
